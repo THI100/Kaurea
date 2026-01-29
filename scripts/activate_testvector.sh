62106256a46ffa434ef55e041f8d32cc3af0d245
Phiphli <128768801+THI100@users.noreply.github.com>
2026-01-29 22:59:57 +0000

    Added a new method to measure memory, time used on the hashing. Found some bugs with the hash content and the size of input in main.c.

--- scripts/activate_testvector.sh
+++ scripts/activate_testvector.sh
@@ -1,5 +1,5 @@
-#use 'chmod +x build.sh' to enable running the file.
+#use 'chmod +x scripts/activate_testvector.sh' to enable running the file.
 
 echo "Compiling..."
 gcc -O3 -march=native -funroll-loops src/flow.c src/formulas.c src/kaurea.c src/solutions.c src/main.c -o Kaurea -lm
-echo "Run: AureaKx.exe for windows or ./AureaKx for linux/mac"
+echo "Run: Kaurea.exe for windows or ./Kaurea for linux/mac"
--- src/main.c
+++ src/main.c
@@ -2,24 +2,46 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/resource.h>
 
 #include "../include/kaurea.h"
 
+void print_memory_usage() {
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    printf("Memory used: %ld kilobytes\n", usage.ru_maxrss);
+}
+
 int main () {
+    // Starting variables
     #define H_LEN 257
 
-    char test[] = "0";
+    clock_t start, end;
+    double cpu_time_used;
+    char test[128] = "";
+    scanf("%s", test);
     size_t size = sizeof(test) / sizeof(test[0]);
     size_t len = 0;
 
     printf("text: %s, size: %zu\n", test, size);
+
+    // Starting hashing/process
+    start = clock();
 
     char* hash_final = malloc(H_LEN * sizeof(char));
     hash_final = hash(test, size, 16, len);
 
     printf("%s\n", hash_final);
 
+    print_memory_usage();
+
+    // Ending process
     free(hash_final);
+
+    end = clock();
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Execution time: %.6f seconds\n", cpu_time_used);
 
     return 0;
 }
